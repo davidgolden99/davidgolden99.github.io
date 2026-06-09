01bca8a2275b22eabd6147c58659c8355cf95014
David Golden <davidgolden@Davids-MacBook-Pro.local>
2026-06-09 15:39:25 -0400

    initial portfolio deploy

---
+++ storyline-demo/story_content/user.js
@@ -0,0 +1,455 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6NPFn5FlYc9":
+        Script1();
+        break;
+      case "6ifdKZ8w1sJ":
+        Script2();
+        break;
+      case "5gThelDVFMV":
+        Script3();
+        break;
+      case "5hm7Ab1fG5y":
+        Script4();
+        break;
+      case "6BARIrRugwu":
+        Script5();
+        break;
+      case "5a0iNouFG4I":
+        Script6();
+        break;
+      case "6FeY50agbLA":
+        Script7();
+        break;
+      case "5VNkHqw2ubk":
+        Script8();
+        break;
+      case "6CSsAYwd3V4":
+        Script9();
+        break;
+      case "6OYoOQAmOxz":
+        Script10();
+        break;
+      case "6coRwtIQiOs":
+        Script11();
+        break;
+      case "5j30aiCfXfS":
+        Script12();
+        break;
+      case "64GOKuQwV1A":
+        Script13();
+        break;
+      case "6mdw4zgtlY3":
+        Script14();
+        break;
+      case "6T1qEyxyRup":
+        Script15();
+        break;
+      case "6IQzNcDIqH5":
+        Script16();
+        break;
+      case "6fGXbNsdYUf":
+        Script17();
+        break;
+      case "666z84K7nQ9":
+        Script18();
+        break;
+      case "6MCa30vgP4Y":
+        Script19();
+        break;
+      case "5lQki9RCgZo":
+        Script20();
+        break;
+      case "5YxU9wtwXnz":
+        Script21();
+        break;
+      case "5kHfn59D7N6":
+        Script22();
+        break;
+      case "63p9qZJcdu0":
+        Script23();
+        break;
+      case "5oY0baPZ4Ki":
+        Script24();
+        break;
+      case "6aOdPSwiwOE":
+        Script25();
+        break;
+      case "65lirhZBBTD":
+        Script26();
+        break;
+      case "6BvLrn0SaFq":
+        Script27();
+        break;
+      case "5fx23kcgkAL":
+        Script28();
+        break;
+      case "5XYekuIM4xr":
+        Script29();
+        break;
+      case "6dYM2MuBLD8":
+        Script30();
+        break;
+      case "6EuwwVvXSA7":
+        Script31();
+        break;
+      case "6TMTJFDULm2":
+        Script32();
+        break;
+      case "5i9e1EMXTzs":
+        Script33();
+        break;
+      case "6RNtDnT9ynq":
+        Script34();
+        break;
+      case "6OdVXyMxAsc":
+        Script35();
+        break;
+      case "6YNeMajGjEv":
+        Script36();
+        break;
+      case "6qWK0JDBZgg":
+        Script37();
+        break;
+      case "5YYn7QoTtoj":
+        Script38();
+        break;
+      case "6QZxvg2b9Dp":
+        Script39();
+        break;
+      case "5xw5DDSvUhD":
+        Script40();
+        break;
+      case "5zrY4jdnAYm":
+        Script41();
+        break;
+      case "6noSd8aVRDh":
+        Script42();
+        break;
+      case "6qKAF77YNfO":
+        Script43();
+        break;
+      case "6M3sD5PUEO1":
+        Script44();
+        break;
+      case "6OD2m1c723i":
+        Script45();
+        break;
+      case "5zteGgxv3Vw":
+        Script46();
+        break;
+      case "6WuI5D6vevq":
+        Script47();
+        break;
+      case "5xblsoA4oIW":
+        Script48();
+        break;
+      case "6dqoqfMkZNW":
+        Script49();
+        break;
+      case "6VbJZyTVh8B":
+        Script50();
+        break;
+      case "6DCYEAgVaRd":
+        Script51();
+        break;
+      case "5me7Uj7e2Y7":
+        Script52();
+        break;
+      case "5nq0b42ibXr":
+        Script53();
+        break;
+      case "6qMNf4B2uIf":
+        Script54();
+        break;
+      case "6EUJG2NCOGy":
+        Script55();
+        break;
+      case "5fYeUeWTuic":
+        Script56();
+        break;
+  }
+}
+
+function Script1()
+{
+  ga('send', 'screenview', {screenName: 'SendPro C - UK - Issue'});
+}
+
+function Script2()
+{
+  ga('send', 'event', 'Button', 'click', 'Wired connection');
+}
+
+function Script3()
+{
+  ga('send', 'event', 'Button', 'click', 'Wireless connection');
+}
+
+function Script4()
+{
+  ga('send', 'event', 'Button', 'click', 'Upload or sync data');
+}
+
+function Script5()
+{
+  ga('send', 'event', 'Button', 'click', 'Refilling postage');
+}
+
+function Script6()
+{
+  ga('send', 'screenview', {screenName: 'Wired check connection'});
+}
+
+function Script7()
+{
+  ga('send', 'event', 'Button', 'click', 'Not connected to local network');
+}
+
+function Script8()
+{
+  ga('send', 'event', 'Button', 'click', 'internet services check is incomplete');
+}
+
+function Script9()
+{
+  ga('send', 'event', 'Button', 'click', 'Connected to local network but not required internet sites');
+}
+
+function Script10()
+{
+  ga('send', 'event', 'Button', 'click', 'Connection good but some required services are unresponsive');
+}
+
+function Script11()
+{
+  ga('send', 'screenview', {screenName: 'Restart the device'});
+}
+
+function Script12()
+{
+  ga('send', 'event', 'Button', 'click', 'Can connect after restart');
+}
+
+function Script13()
+{
+  ga('send', 'event', 'Button', 'click', 'Can not connect after restart');
+}
+
+function Script14()
+{
+  ga('send', 'screenview', {screenName: 'Check physical connection'});
+}
+
+function Script15()
+{
+  ga('send', 'event', 'Button', 'click', 'Ethernet port has lights');
+}
+
+function Script16()
+{
+  ga('send', 'event', 'Button', 'click', 'Ethernet port does not have lights');
+}
+
+function Script17()
+{
+  ga('send', 'screenview', {screenName: 'Ethernet has lights'});
+}
+
+function Script18()
+{
+  ga('send', 'event', 'Button', 'click', 'Can connect');
+}
+
+function Script19()
+{
+  ga('send', 'event', 'Button', 'click', 'Can not connect');
+}
+
+function Script20()
+{
+  ga('send', 'event', 'Button', 'click', 'Different light pattern on Ethernet port');
+}
+
+function Script21()
+{
+  ga('send', 'screenview', {screenName: 'Try a WiFi connection'});
+}
+
+function Script22()
+{
+  ga('send', 'event', 'Button', 'click', 'Can connect on WiFi');
+}
+
+function Script23()
+{
+  ga('send', 'event', 'Button', 'click', 'Can not connect on WiFi');
+}
+
+function Script24()
+{
+  ga('send', 'event', 'Button', 'click', 'No WiFi available');
+}
+
+function Script25()
+{
+  ga('send', 'screenview', {screenName: 'Check Ethernet port'});
+}
+
+function Script26()
+{
+  ga('send', 'event', 'Button', 'click', 'Ethernet port active');
+}
+
+function Script27()
+{
+  ga('send', 'event', 'Button', 'click', 'Ethernet port inactive');
+}
+
+function Script28()
+{
+  ga('send', 'event', 'Button', 'click', 'Not sure');
+}
+
+function Script29()
+{
+  ga('send', 'screenview', {screenName: 'SendPro C - UK - Tech support'});
+}
+
+function Script30()
+{
+  ga('send', 'event', 'Button', 'click', 'Support', '1');
+}
+
+function Script31()
+{
+  ga('send', 'screenview', {screenName: 'Work with IT'});
+}
+
+function Script32()
+{
+  ga('send', 'event', 'Button', 'click', 'Can connect');
+}
+
+function Script33()
+{
+  ga('send', 'event', 'Button', 'click', 'Can not connect');
+}
+
+function Script34()
+{
+  ga('send', 'screenview', {screenName: 'Wireless check connection'});
+}
+
+function Script35()
+{
+  ga('send', 'event', 'Button', 'click', 'Not connected to local network');
+}
+
+function Script36()
+{
+  ga('send', 'event', 'Button', 'click', 'internet services check is incomplete');
+}
+
+function Script37()
+{
+  ga('send', 'event', 'Button', 'click', 'Connected to local network but not required internet sites');
+}
+
+function Script38()
+{
+  ga('send', 'event', 'Button', 'click', 'Connection good but some required services are unresponsive');
+}
+
+function Script39()
+{
+  ga('send', 'screenview', {screenName: 'SendPro C - UK - Resolved'});
+}
+
+function Script40()
+{
+  ga('send', 'event', 'Button', 'click', 'SendPro C - UK - Resolved', '1');
+}
+
+function Script41()
+{
+  ga('send', 'screenview', {screenName: 'Try another WiFi connection'});
+}
+
+function Script42()
+{
+  ga('send', 'event', 'Button', 'click', 'No other WiFi network available');
+}
+
+function Script43()
+{
+  ga('send', 'event', 'Button', 'click', 'Can not connect on other WiFi network');
+}
+
+function Script44()
+{
+  ga('send', 'event', 'Button', 'click', 'Can connect on other WiFi network');
+}
+
+function Script45()
+{
+  ga('send', 'screenview', {screenName: 'Check available postage'});
+}
+
+function Script46()
+{
+  ga('send', 'event', 'Button', 'click', 'Can refill after adding funds');
+}
+
+function Script47()
+{
+  ga('send', 'event', 'Button', 'click', 'Can not refill after adding funds');
+}
+
+function Script48()
+{
+  ga('send', 'screenview', {screenName: 'Upload or sync issues'});
+}
+
+function Script49()
+{
+  ga('send', 'event', 'Button', 'click', 'Wired connection');
+}
+
+function Script50()
+{
+  ga('send', 'event', 'Button', 'click', 'Wireless connection');
+}
+
+function Script51()
+{
+  ga('send', 'screenview', {screenName: 'Reset wired connection'});
+}
+
+function Script52()
+{
+  ga('send', 'event', 'Button', 'click', 'Can connect after reset Ethernet connection');
+}
+
+function Script53()
+{
+  ga('send', 'event', 'Button', 'click', 'Can not connect after reset Ethernet connection');
+}
+
+function Script54()
+{
+  ga('send', 'screenview', {screenName: 'Reset wireless connection'});
+}
+
+function Script55()
+{
+  ga('send', 'event', 'Button', 'click', 'Can connect after reset WiFi connection');
+}
+
+function Script56()
+{
+  ga('send', 'event', 'Button', 'click', 'Can not connect after reset WiFi connection');
+}
+
